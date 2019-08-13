955fca3d397ce9c53d6d1e11150d98eca40b60a9
joyytoo <joytoo97@gmail.com>
2019-08-13 17:42:25 +0300

    Make components dynamic

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 // react creates js objects that describe dom nodes
 // react doesn't touch actual dom directly, it just managers what get rendered to the DOM
 // it is renders job to interpret and create elements
+// props is that they are "read only" (or immutable), which means that a component can only read the props given to it,
+//  never change them. The (parent) component higher in the tree owns and controls the property values.
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header>
-      <h1>ScoreBoard</h1>
-      <span className="stats">Players: 1</span>
+      <h1>{ props.title }</h1>
+      <span className="stats">Players: { props.totalPlayers }</span>
     </header>
   );
 };
 
-const Player = () => {
+const Player = (props) => {
   return (
     <div className="player">
       <span className="player-name">
-        Joy
+        { props.name }
       </span>
 
       <Counter />
@@ -23,24 +25,75 @@ const Player = () => {
   )
 };
 
-const Counter = () => {
-  return (
-    <div className="counter">
-      <button className="counter-action decrement"> - </button>
-      <span className="counter-score">35</span>
-      <button className="counter-action increment"> + </button>
-    </div>
-  )
-};
+class Counter extends React.Component {
 
-const App = () => {
-  return (
-    <div className="scoreboard">
-      <Header/>
-      <Player/>
-    </div>
-  );
-};
+  state = {
+    score: 0
+  };
+
+  incrementScore = () => {
+    this.setState( prevState => ({
+      score: prevState.score + 1
+    }));
+  };
+
+  decrementScore = () => {
+    this.setState( prevState => ({
+      score: prevState.score - 1
+    }))
+  };
+
+  render() {
+    return (
+      <div className="counter">
+        <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
+        <span className="counter-score">{ this.state.score }</span>
+        <button className="counter-action increment" onClick={this.incrementScore}> + </button>
+      </div>
+    )
+  }
+}
+
+class App extends React.Component {
+
+  state = {
+    players:  [
+      {
+        name: "Joy",
+        id: 1
+      },
+      {
+        name: "Treasure",
+        id: 2
+      },
+      {
+        name: "Ashley",
+        id: 3
+      },
+      {
+        name: "James",
+        id: 4
+      }
+    ]
+  };
+
+  render() {
+    return (
+      <div className="scoreboard">
+        <Header
+          title="Scoreboard"
+          totalPlayers={this.state.players.length} />
+
+        {this.state.players.map( player =>
+          <Player
+            name={player.name}
+            key={player.id.toString()}
+          />
+        )}
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
   <App />, // element to render
